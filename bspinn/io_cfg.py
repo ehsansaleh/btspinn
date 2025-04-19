fe597d84e2219b854ba746976fecfc97642afa6e
ehsansaleh <ehsan.slh@gmail.com>
2025-04-19 17:49:04 -0500

    global project path variable definitions

--- bspinn/io_cfg.py
+++ bspinn/io_cfg.py
@@ -1,20 +1,19 @@
 import os
 from textwrap import dedent
 
-projname = 'code_bspinn'
 cwd_list = os.getcwd().split(os.sep)
-msg = f"""
+projname = 'btspinn' if ('code_bspinn' not in cwd_list) else 'code_bspinn'
+assert cwd_list.count(projname) == 1, dedent(f"""
     The project name "{projname}" must appear exactly
     once in the current working directory:
-        >> os.getcwd() == {os.getcwd()}"""
-assert cwd_list.count(projname) == 1, dedent(msg)
+        >> os.getcwd() == {os.getcwd()}""")
 PROJPATH = os.sep.join(cwd_list[:cwd_list.index(projname) + 1])
 
 configs_dir = f'{PROJPATH}/configs'
 results_dir = f'{PROJPATH}/results'
 storage_dir = f'{PROJPATH}/storage'
 summary_dir = f'{PROJPATH}/summary'
-source_dir = f'{PROJPATH}/{projname.replace("code_", "")}'
+source_dir = f'{PROJPATH}/bspinn'
 
 # The key specs used for summarization
 keyspecs = [('hp',         'last'),
